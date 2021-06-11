e9bf2639468606bab6e6d609dc6e072f3a7a1fdd
lexmarcos <marcosuel.vieira@hotmail.com>
2021-06-11 16:02:48 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="w-full h-screen flex items-center justify-center home-container">
+        <p class="text-9xl font-black tracking-wider">20:00</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Home"
+}
+</script>
+
+<style scoped>
+    .home-container{
+        background-color: #FBFAF3;
+    }
+</style>
